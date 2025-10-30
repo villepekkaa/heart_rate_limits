a5a51b9a009642c1e54753f517a0ce2232e4453b
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-10-30 08:38:54 +0200

    tyylejä ja inputteja

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
 
+  const [age, setAge] = useState<string>("")
+  const ageAsNumber: number = !isNaN(Number(age))===true ? Number(age) : 0
 
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <Text style = {styles.heading}>Heart Rate Limits Calculator</Text>
+      <Text>Enter your age:</Text>
+      <TextInput
+      keyboardType='number-pad'
+      style={styles.field}
+      value={age}
+      onChangeText={setAge}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -24,4 +33,7 @@ const styles = StyleSheet.create({
   heading: {
     fontSize: 28
   },
+  field: {
+    marginVertical: 8
+  }
 });
