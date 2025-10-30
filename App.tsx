8b02a356d270d97c7b554f22932e1baf3145ca63
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-10-30 08:24:40 +0200

    harjoitus aloitettu

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style = {styles.heading}>Heart Rate Limits Calculator</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +20,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 28
   },
 });
