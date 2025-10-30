443f12b2951054fa92d9f85d9bec6f1c74965be3
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-10-30 09:26:03 +0200

    tehtävä valmis

--- App.tsx
+++ App.tsx
@@ -5,19 +5,28 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 export default function App() {
 
   const [age, setAge] = useState<string>("")
-  const ageAsNumber: number = !isNaN(Number(age))===true ? Number(age) : 0
+  const ageAsNumber: number = Number(age)
+  const isValidNumber: boolean = age !== "" && !isNaN(ageAsNumber)
 
+  const lowerLimit: number = isValidNumber ? (220-ageAsNumber) * 0.65 : 0
+  const upperLimit: number = isValidNumber ? (220-ageAsNumber) * 0.85 : 0
 
   return (
     <View style={styles.container}>
       <Text style = {styles.heading}>Heart Rate Limits Calculator</Text>
       <Text>Enter your age:</Text>
       <TextInput
-      keyboardType='number-pad'
-      style={styles.field}
-      value={age}
-      onChangeText={setAge}
+        keyboardType='number-pad'
+        style={styles.inputField}
+        value={age}
+        onChangeText={setAge}
       />
+      {age !== "" && (
+        <>
+          <Text style={styles.field}>Lower limit: {lowerLimit.toFixed(2)} bpm</Text>
+          <Text style={styles.field}>Upper limit: {upperLimit.toFixed(2)} bpm</Text>
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -27,13 +36,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    paddingTop: 100,
+    paddingLeft: 10
   },
   heading: {
-    fontSize: 28
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
   },
   field: {
-    marginVertical: 8
+    
+  },
+  inputField: {
+    marginVertical: 8,
+    borderWidth: 1,
+    borderColor: '#333',
+    borderRadius: 6,
+    paddingRight: 40,
+    paddingLeft: 8,
+    paddingVertical: 8,
+    textAlign: 'left',
   }
 });
